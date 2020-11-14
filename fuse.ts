0afabf5e32ae78748903237fd1fcced1bbd7b47d
ayumitamai97 <ayumitamai97@gmail.com>
2020-11-15 01:20:07 +0900

    Add background script

--- fuse.ts
+++ fuse.ts
@@ -1,18 +1,24 @@
-import { FuseBox } from 'fuse-box'
+import { FuseBox, SassPlugin, CSSPlugin } from 'fuse-box'
 
 const fuse = FuseBox.init({
   homeDir: 'src',
   entry: 'src/showDialog.tsx',
   target: 'browser',
   output: 'dist/$name.js',
-  devServer: this.runServer
+  devServer: this.runServer,
+  plugins: [
+    [
+      SassPlugin(),
+      CSSPlugin()
+    ]
+  ]
 })
 
 const showDialog = fuse.bundle('showDialog').instructions(' > showDialog.tsx')
-const shopping = fuse.bundle('Dialog').instructions(' > components/Dialog.tsx')
+const background = fuse.bundle('pageChanged').instructions(' > pageChanged.ts')
 
 if (process.argv.includes('--hmr')) {
-  [showDialog, shopping].map(script => script.hmr().watch())
+  [showDialog, background].map(script => script.hmr().watch())
 }
 
 fuse.run()
