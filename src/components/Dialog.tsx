03d53aed982ee298d9b43c0f6199f7bcf4d2fdcb
ayumitamai97 <ayumitamai97@gmail.com>
2020-11-14 19:03:04 +0900

    Add components

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div>
+        This will be a dialog :)
+      </div>
+    )
+  }
+}
---
+++ src/showDialog.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Dialog from './components/Dialog'
+
+ReactDOM.render(
+  <Dialog />,
+  document.getElementById('commits_bucket')
+)
