7d13a5088019248e92af79746a48ba60fb5cb35b
ayumitamai97 <ayumitamai97@gmail.com>
2020-12-06 15:56:07 +0900

    Fix bug: issue selector not showing after the second time

--- src/showDialog.tsx
+++ src/showDialog.tsx
@@ -4,30 +4,49 @@ import App from './components/App'
 import { Message } from './@types/message'
 
 const multeseRootId = 'multeseRoot'
+const createMulteseRoot = (): HTMLElement => {
+  const multeseRoot = document.getElementById(multeseRootId) || document.createElement('div')
+  multeseRoot.setAttribute('id', multeseRootId)
+  return multeseRoot
+}
 
 const onPageChangedToComparePage = (message: Message, _sender, _callback) => {
   if (message.pageType) {
-    const multeseRoot = document.getElementById(multeseRootId) || document.createElement('div')
-    multeseRoot.setAttribute('id', multeseRootId)
+    const multeseRoot = createMulteseRoot()
 
-    const targetNodeSelectorMapper = {
-      compare: 'body',
-      project: '.js-convert-note-to-issue-form'
-    }
+    if (message.pageType === 'compare') {
+      const targetNode = document.querySelector('body')
+      targetNode.appendChild(multeseRoot)
 
-    const timer = setInterval(() => {
-      const targetNode = document.querySelector(targetNodeSelectorMapper[message.pageType])
-      if (targetNode) {
-        clearInterval(timer)
-        if (document.getElementById(multeseRootId)) { return }
-        targetNode.appendChild(multeseRoot)
+      ReactDOM.render(
+        <App pageType={message.pageType} />,
+        document.getElementById(multeseRootId)
+      )
+    } else if (message.pageType === 'project') {
+      const renderIssueTemplateSelector = () => {
+        const dataDialogId = (event.target as HTMLElement).getAttribute('data-dialog-id')
+        if (dataDialogId && dataDialogId.match(/^convert-to-issue-/)) {
+          const multeseRoot = createMulteseRoot()
 
-        ReactDOM.render(
-          <App pageType={message.pageType} />,
-          document.getElementById(multeseRootId)
-        )
+          const timer = setInterval(() => {
+            const targetNode = document.querySelector('.js-convert-note-to-issue-form')
+            if (targetNode) {
+              clearInterval(timer)
+              if (document.getElementById(multeseRootId)) { return }
+              targetNode.appendChild(multeseRoot)
+
+              ReactDOM.render(
+                <App pageType={message.pageType} />,
+                document.getElementById(multeseRootId)
+              )
+            }
+          }, 50)
+        }
       }
-    }, 50)
+
+      const columnContainer = document.querySelector('.project-columns-container')
+      columnContainer.addEventListener('click', renderIssueTemplateSelector)
+    }
   } else {
     const oldMulteseRoot = document.getElementById(multeseRootId)
     if (!oldMulteseRoot) { return }
